679ded5b832570dddf3f6adf83230ee769205cd7
SHREYASH KUMAR AGNIHOTRI <84907832+shreyash2509@users.noreply.github.com>
2023-06-22 15:24:30 +0530

    Time: 0 ms (100%), Space: 6 MB (63.81%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string defangIPaddr(string s) {
+        string ans;
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '.'){
+                ans+="[.]";
+            }
+            else{
+                ans+=s[i];
+            }
+        }
+        return ans;
+    }
+};
