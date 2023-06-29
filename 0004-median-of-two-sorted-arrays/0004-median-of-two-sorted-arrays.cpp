726ce136d4f95b3c6805d6b07be83d6bfb49aaad
SHREYASH KUMAR AGNIHOTRI <84907832+shreyash2509@users.noreply.github.com>
2023-06-29 14:24:19 +0530

    Time: 51 ms (32.23%), Space: 89.3 MB (79.04%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> v;
-        
-        for (int num : nums1) {
-            v.insert(lower_bound(v.begin(), v.end(), num), num);
-        }
-        for (int num : nums2) {
-            v.insert(lower_bound(v.begin(), v.end(), num), num);
-        }
-        
-        int x = v.size();
-        
-        if (x % 2 == 0) {
-            return (v[x/2 - 1] + v[x/2]) / 2.0;
-        } else {
-            return v[x/2];
-        }
+    double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
+    
+        int n1 = A.size();
+        int n2 = B.size();
+        int n = n1 + n2;
+        if(n1 > n2)
+            return findMedianSortedArrays(B,A);
+        int partition = (n + 1)/2;
+        int start = 0 , end = n1;
+         while(start <= end)
+         {
+             int mid1 = (end + start)/2;
+             int mid2 = partition - mid1;
+             int leftA = (mid1 <= 0) ? INT_MIN : A[mid1-1];
+             int leftB = (mid2 <= 0) ? INT_MIN : B[mid2-1];
+             int rightA = (mid1 < n1) ? A[mid1] : INT_MAX;
+             int rightB = (mid2 < n2) ? B[mid2] : INT_MAX;
+             if(leftA <= rightB && leftB <= rightA)
+             {
+                 if((n1+n2)%2 == 1)
+                 {
+                     return max(leftA,leftB);
+                 }
+                 else
+                 {
+                     return (max(leftA,leftB) + min(rightA , rightB))/2.0;
+                 }
+             }
+             else if(leftA > rightB)
+             {
+                 end = mid1 - 1;
+             }
+             else
+             {
+                 start = mid1 + 1;
+             }
+         }
+         return 0.0;
     }
 };
