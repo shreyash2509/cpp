6aac8fa62f234791f526e2dfab1904e4e438d491
SHREYASH KUMAR AGNIHOTRI <84907832+shreyash2509@users.noreply.github.com>
2023-07-07 17:56:55 +0530

    Time: 21 ms (91.34%), Space: 13.3 MB (50.48%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,40 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode *list3, *hd;
-        hd = new ListNode();
-        list3 = hd;
-        int x = lists.size();
-        
-        while (!lists.empty()) {
-            vector<int> arr(x, INT_MAX);
-            
-            for (int i = 0; i < x; i++) {
-                if (lists[i]) {
-                    arr[i] = lists[i]->val;
-                } else {
-                    arr[i] = INT_MAX; 
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL) return list2;   
+        if(list2 == NULL) return list1;
+
+        ListNode *head =NULL,*tail =NULL;
+        while(list1 and list2)
+        {
+            if(list1->val<list2->val)
+            {
+                if(head == NULL)
+                {
+                    head = list1;
+                    tail = list1;
                 }
+                else{
+                    tail->next = list1;
+                    tail = tail->next;
+                }
+                list1 = list1->next;
             }
-            
-            int minValue = *min_element(arr.begin(), arr.end());
-            int index = find(arr.begin(), arr.end(), minValue) - arr.begin();
-            
-            if (minValue == INT_MAX) {
-                break;
-            }
-            
-            list3->next = lists[index];
-            list3 = list3->next;
-            
-            lists[index] = lists[index]->next;
-            
-            if (!lists[index]) {
-                lists.erase(lists.begin() + index);
-                x--;
+            else{
+                if(head == NULL)
+                {
+                    head = list2;
+                    tail = list2;
+                }
+                else{
+                    tail->next = list2;
+                    tail = tail->next;
+                }
+                list2 = list2->next;
             }
         }
-        
-        return hd->next;
+        if(list1)
+        {
+            tail->next = list1;
+        }
+        if(list2)
+        {
+            tail->next = list2;
+        }
+        return head;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        //merge all 
+        // divide and conquer
+        if(lists.size() == 0)
+        {
+            return NULL;
+        }
+        ListNode *head = NULL;
+        vector<ListNode*>list(lists.begin(),lists.end());
+        while(list.size()>1)
+        {
+            vector<ListNode*>l;
+            int n = list.size();
+            // cout<<n<<"\n";
+            for(int i = 1;i<n;i+=2)
+            {
+                ListNode *t = mergeTwoLists(list[i],list[i-1]);
+                l.push_back(t);
+            }
+            if(n%2!=0)
+            {
+                l.push_back(list[n-1]);
+            }
+            swap(l,list);
+        }
+        return list[0];
     }
 };
