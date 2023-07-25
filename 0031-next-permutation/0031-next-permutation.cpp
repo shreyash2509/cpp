5855f32843caa1a4532d1dae533451f5426e55fb
SHREYASH KUMAR AGNIHOTRI <84907832+shreyash2509@users.noreply.github.com>
2023-07-26 01:53:11 +0530

    Time: 4 ms (72.25%), Space: 11.9 MB (92.31%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n = nums.size(); // size of the array.
+        int ind = -1; // break point
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                // index i is the break point
+                ind = i;
+                break;
+            }
+        }
+
+        // If break point does not exist:
+        if (ind == -1) {
+            // reverse the whole array:
+            reverse(nums.begin(), nums.end());
+        } else {
+            // Step 2: Find the next greater element
+            //         and swap it with nums[ind]:
+            for (int i = n - 1; i > ind; i--) {
+                if (nums[i] > nums[ind]) {
+                    swap(nums[i], nums[ind]);
+                    break;
+                }
+            }
+
+            // Step 3: reverse the right half:
+            reverse(nums.begin() + ind + 1, nums.end());
+        }
     }
 };
